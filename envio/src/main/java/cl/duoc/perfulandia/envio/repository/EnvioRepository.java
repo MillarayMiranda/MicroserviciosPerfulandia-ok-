336e3a0542e0a7946d2e3bb3f48f2525d9532966
Mess-Dev <diego.s.araya.s@gmail.com>
2025-06-05 21:03:45 -0400

    repository añadido

---
+++ envio/src/main/java/cl/duoc/perfulandia/envio/repository/EnvioRepository.java
@@ -0,0 +1,9 @@
+package cl.duoc.perfulandia.envio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.duoc.perfulandia.envio.model.Envio;
+
+public interface EnvioRepository extends JpaRepository<Envio, Long>{
+
+}
