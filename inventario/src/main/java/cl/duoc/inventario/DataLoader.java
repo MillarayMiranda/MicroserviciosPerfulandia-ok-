56c22b3fd6ea7d6248ab4605c921b3a839e63240
18mili <mi.honorio@duocuc.cl>
2025-06-07 13:11:59 -0400

    prueba

--- inventario/src/main/java/cl/duoc/inventario/DataLoader.java
+++ inventario/src/main/java/cl/duoc/inventario/DataLoader.java
@@ -2,11 +2,12 @@ package cl.duoc.inventario;
 
 import java.util.Locale;
 import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import net.datafaker.Faker;
 
+import net.datafaker.Faker;
 
 import cl.duoc.inventario.model.Inventario;
 import cl.duoc.inventario.services.InventarioService;
@@ -14,7 +15,9 @@ import cl.duoc.inventario.services.InventarioService;
 @Component
 public class DataLoader implements CommandLineRunner {
 
-    private final Faker faker = new Faker(new Locale("es", "CL"));
+    private final Faker faker = new Faker(
+        new Locale.Builder().setLanguage("es").setRegion("CL").build()
+    );
     private final Random random = new Random();
 
     @Autowired
@@ -30,10 +33,10 @@ public class DataLoader implements CommandLineRunner {
             nuevoInventario.setSucursalId((long) (random.nextInt(10) + 1)); // ID de sucursal entre 1 y 10
             nuevoInventario.setProductoId((long) (random.nextInt(50) + 1)); // ID de producto entre 1 y 50
             inventarioService.crearInventario(nuevoInventario);
+
             System.out.println("Inventario creado: Cantidad=" + nuevoInventario.getCantidad() +
                                ", SucursalID=" + nuevoInventario.getSucursalId() +
                                ", ProductoID=" + nuevoInventario.getProductoId());
         }
     }
-
 }
