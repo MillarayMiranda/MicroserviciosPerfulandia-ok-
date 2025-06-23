e646e42534661cd97959251db71db57163bd62c5
Mess-Dev <diego.s.araya.s@gmail.com>
2025-06-23 07:26:19 -0400

    model login agregado

--- login/src/main/java/cl/duoc/perfulandia/login/model/Login.java
+++ login/src/main/java/cl/duoc/perfulandia/login/model/Login.java
@@ -1,5 +1,23 @@
 package cl.duoc.perfulandia.login.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="LOGIN")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Login {
+
+    @Column(name = "mail", nullable = false)
+    private String correo;
+    
+    @Column(name = "password", nullable = false)
+    private String password;
 
 }
